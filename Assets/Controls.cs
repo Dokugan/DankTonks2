df524bc2e8bdadb1c5a66ea58f69846fff3ebd51
Jaap-Jan de Wit <jipjan@gmail.com>
2017-10-16 19:32:16 +0200

    camera and movementspeed improvements

--- Assets/Controls.cs
+++ Assets/Controls.cs
@@ -2,15 +2,13 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Controls : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
-		
-	}
+public class Controls : MonoBehaviour
+{
+    public float MovementSpeed = 2f;
 	
-	// Update is called once per frame
-	void Update () {
-		
+	void Update ()
+	{
+	    var x = Input.GetAxis("Horizontal");
+	    transform.position += new Vector3(x, 0, 0) * MovementSpeed * Time.deltaTime;
 	}
 }
