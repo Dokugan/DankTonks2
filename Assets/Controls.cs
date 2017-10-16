7c8c2820dd972e8061d58a85c65427177f1a7f4d
Jaap-Jan de Wit <jipjan@gmail.com>
2017-10-16 20:47:40 +0200

    rotation shit die nog gefixt moet worden (Barrel)

--- Assets/Controls.cs
+++ Assets/Controls.cs
@@ -1,14 +1,35 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Controls : MonoBehaviour
 {
     public float MovementSpeed = 2f;
-	
+    public float GunRotation = 3f;
+
+    public Transform RotationPoint;
+    
 	void Update ()
 	{
 	    var x = Input.GetAxis("Horizontal");
+	    var y = Input.GetAxis("Vertical");
 	    transform.position += new Vector3(x, 0, 0) * MovementSpeed * Time.deltaTime;
-	}
+
+	    RotationPoint.Rotate(new Vector3(0, 0, 90 * y), Time.deltaTime * GunRotation);
+        if (RotationPoint.localEulerAngles.z > 90)
+            RotationPoint.localEulerAngles = new Vector3(0, 0, 90);
+        //else
+        //if (RotationPoint.localEulerAngles.z < -90)
+        //RotationPoint.localEulerAngles = new Vector3(0, 0, -90);
+        /*
+        if (y != 0)
+        {
+            rot.z += y * GunRotation * Time.deltaTime;
+            RotationPoint.rotation = rot;
+
+            Debug.Log(RotationPoint.rotation.eulerAngles.z);
+        }
+        */
+    }
 }
