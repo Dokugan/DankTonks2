16be19a17b062197d98cc1a5755681d50f3c02f9
Stijn van Gils <stijn-28@hotmail.com>
2017-10-17 16:41:59 +0200

    Terrain is now always on the bottom of the viewport

--- Assets/FocusCamera.cs
+++ Assets/FocusCamera.cs
@@ -18,8 +18,9 @@ public class FocusCamera : MonoBehaviour
         var cam = Camera.main;
         var p = cam.ScreenToWorldPoint(new Vector3(0, cam.pixelHeight, cam.nearClipPlane));
         cam.orthographicSize = _renderer.bounds.size.x  * Screen.height / Screen.width * 0.5f;
-
+        
         cam.transform.position = new Vector3(_renderer.bounds.center.x, _renderer.bounds.max.y, cam.transform.position.z);
+        cam.transform.position = new Vector3(cam.transform.position.x, cam.orthographicSize, cam.transform.position.z);
     }
 
 }
