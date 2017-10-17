ac2d246e86fd6df3302ddc96659dc0d5d3a9762e
Jaap-Jan de Wit <jipjan@gmail.com>
2017-10-17 15:20:34 +0200

    camera and terrain generation fixed

--- Assets/Scripts/TerrainManager.cs
+++ Assets/Scripts/TerrainManager.cs
@@ -5,21 +5,19 @@ namespace Assets.Scripts
 {
     public class TerrainManager : MonoBehaviour
     {
-
         public int InitPoints = 8;
-        public float MostRightX = 50f;
-        public float MaxY = 5f;
-        public float MinY = 2f;
         public int Iterations = 5;
 
         // Use this for initialization
         void Start ()
         {
+            var cam = Camera.main;
+            var p = cam.ScreenToWorldPoint(new Vector3(0, cam.pixelHeight, cam.nearClipPlane));
 
             var mf = GetComponent<MeshFilter>();
             var mesh = mf.mesh;
 
-            var terrainGenerator = new TerrainGenerator(InitPoints, MostRightX, MaxY, MinY, Iterations);
+            var terrainGenerator = new TerrainGenerator(InitPoints, Mathf.Abs(p.x * 2), Mathf.Abs(p.y), Mathf.Abs(p.y) / 2, Iterations);
 
             mesh.Clear();
             mesh.vertices = terrainGenerator.Vertices;
@@ -27,13 +25,8 @@ namespace Assets.Scripts
             mesh.RecalculateNormals();
 
             gameObject.AddComponent<MeshCollider>();
-//            foreach (var tri in terrainGenerator.Vertices)
-//            {
-//                Debug.Log(tri);
-//            }
-//
-//            Debug.Log(terrainGenerator.Triangles);
-//            Debug.Log(terrainGenerator.Vertices.ToString());
+
+            transform.position = new Vector3(p.x, -p.y, transform.position.z);
         } 
 	
         // Update is called once per frame
