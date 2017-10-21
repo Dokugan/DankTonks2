ea040c62912c6e0d3d20bdcf3ef406119a6a96c2
Stijn van Gils <stijn-28@hotmail.com>
2017-10-21 14:12:26 +0200

    Gave depth to terrain and replaced sprite tank with 3D tank

---
+++ Assets/Scripts/TankControls.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TankControls : MonoBehaviour
+{
+    public float MaxTorque = 25f;
+
+    public WheelCollider[] WheelColliders = new WheelCollider[5];
+    public Transform[] TireMeshes = new Transform[5];
+
+	// Use this for initialization
+	void Start () {
+
+        foreach (var wheel in WheelColliders)
+        {
+            wheel.steerAngle = 90;
+        }
+		
+	}
+	
+	// Update is called once per frame
+    void Update()
+    {
+        UpdateMeshesPositions();
+    }
+
+	void FixedUpdate ()
+	{
+
+	    float accelerate = Input.GetAxis("Horizontal");
+
+	    for (int i = 0; i < 5; i++)
+	    {
+	        WheelColliders[i].motorTorque = accelerate * MaxTorque;
+	    }
+
+    }
+
+    void UpdateMeshesPositions()
+    {
+        for (int i = 0; i < 5; i++)
+        {
+            Quaternion quat;
+            Vector3 pos;
+            WheelColliders[i].GetWorldPose(out pos, out quat);
+
+            TireMeshes[i].position = pos;
+            //TireMeshes[i].rotation = quat;
+        }
+    }
+}
--- Assets/Scripts/TerrainManager.cs
+++ Assets/Scripts/TerrainManager.cs
@@ -14,8 +14,7 @@ namespace Assets.Scripts
         public float MaxHeight = 8f;
         private Vector3[] _vertices;
         private int[] _triangles;
-        private List<Point2D> _pointsList;
-        
+        private List<Point3D> _pointsList;
 
         // Use this for initialization
         void Start ()
@@ -33,6 +32,10 @@ namespace Assets.Scripts
 
             mesh.Clear();
             mesh.vertices = _vertices;
+            foreach (var vari in _triangles)
+            {
+                print(vari);
+            }
             mesh.triangles = _triangles;
             mesh.RecalculateNormals();
 
@@ -40,18 +43,18 @@ namespace Assets.Scripts
 
         }
 
-        private List<Point2D> GenerateTerrain([CanBeNull] List<Point2D> points, int iterations)
+        private List<Point3D> GenerateTerrain([CanBeNull] List<Point3D> points, int iterations)
         {
             if (points == null)
             {
-                points = new List<Point2D>();
+                points = new List<Point3D>();
                 var increment = MaxX / (InitPoints - 1);
                 var posX = 0f;
 
                 for (var i = 0; i < InitPoints; i++)
                 {
                     var height = MinHeight + (UnityEngine.Random.value * (MaxHeight - MinHeight));
-                    points.Add(new Point2D(posX, height));
+                    points.Add(new Point3D(posX, height,0));
                     posX += increment;
                 }
             }
@@ -77,23 +80,27 @@ namespace Assets.Scripts
                     }
 
                     var pointY = pointYMin + UnityEngine.Random.value * (pointYMax - pointYMin);
-                    points.Insert(i + 1, new Point2D(pointxSpacing * (i + 1), pointY));
+                    points.Insert(i + 1, new Point3D(pointxSpacing * (i + 1), pointY,0));
                 }
                 GenerateTerrain(points, iterations - 1);
             }
             return points;
         }
 
-        private Vector3[] ToVectorArray(List<Point2D> points)
+        private Vector3[] ToVectorArray(List<Point3D> points)
         {
-            var vectors = new Vector3[points.Count * 2];
+            var vectors = new Vector3[points.Count * 4];
             var index = 0;
+            var reverse = vectors.Length -2;
 
             foreach (var point in points)
             {
                 vectors[index] = new Vector3(point.PosX, 0, 0);
                 vectors[index + 1] = new Vector3(point.PosX, point.PosY, 0);
+                vectors[reverse] = new Vector3(point.PosX, 0, 1);
+                vectors[reverse + 1] = new Vector3(point.PosX, point.PosY, 1);
                 index += 2;
+                reverse -= 2;
             }
 
             return vectors;
@@ -101,8 +108,11 @@ namespace Assets.Scripts
 
         private int[] CreateTriangles(Vector3[] vertices)
         {
-            var triangles = new int[vertices.Length * 3];
+            //+ (_pointsList.Count -1) * 2 * 3
+            var triangles = new int[vertices.Length * 3 + (_pointsList.Count - 1) * 2 * 3];
             int index = 0;
+
+            Debug.Log(vertices.Length + "   " + _pointsList.Count);
 
             for (var i = 0; i < vertices.Length - 2; i++)
             {
@@ -120,9 +130,36 @@ namespace Assets.Scripts
                 }
                 index += 3;
             }
+            
+            Debug.Log(index + "  " + triangles.Length);
+
+            for (var j = 1; j < _pointsList.Count * 2 ; j += 2)
+            {
+                triangles[index] = j;
+                triangles[index + 1] = vertices.Length - j ;
+                triangles[index + 2] = vertices.Length - j - 2;
+                triangles[index + 3] = vertices.Length - j - 2;
+                triangles[index + 4] = j + 2;
+                triangles[index + 5] = j;
+
+                index += 6;
+            }
+
+            Debug.Log(triangles[index - 1]);
 
             return triangles;
         }
+
+//        private void CreateCollider()
+//        {
+//            for (var i = 0; i < _pointsList.Count; i += 2)
+//            {
+//                _pointsList[i].PosX = _pointsList[i + 1].PosX;
+//                _pointsList[i].PosY = _pointsList[i + 1].PosY;
+//                _pointsList[i].PosZ = -1;
+//                _pointsList[i + 1].PosZ = 1;
+//            }
+//        }
 
         public void CalculateImpact(float x, float y, float impactRadius)
         {
@@ -154,15 +191,17 @@ namespace Assets.Scripts
         }
     }
 
-    public class Point2D
+    public class Point3D
     {
         public float PosX { get; set; }
         public float PosY { get; set; }
+        public float PosZ { get; set; }
 
-        public Point2D(float posX, float posY)
+        public Point3D(float posX, float posY, float posZ)
         {
             PosX = posX;
             PosY = posY;
+            PosZ = posZ;
         }
 
         public override string ToString()
