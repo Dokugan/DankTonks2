ddb56720697305fcf9a40f69312975ca4a73eb6e
Stijn van Gils <stijn-28@hotmail.com>
2017-10-22 17:12:47 +0200

    Started work on networking functionality

---
+++ Assets/Scripts/NetworkManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Assets.Scripts;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class NetworkManager : UnityEngine.Networking.NetworkManager
+{
+    public GameObject Terrain;
+
+    private float _spawnOffset = 1f;
+
+    public override void OnStartServer()
+    {
+        base.OnStartServer();
+        NetworkServer.Spawn(Terrain);
+    }
+
+    public override void OnServerAddPlayer(NetworkConnection conn, short playerControllerId)
+    {
+        float spawnX;
+        if (numPlayers % 2 == 0)
+        {
+            spawnX = numPlayers + 1 * _spawnOffset;
+        }
+        else
+        {
+            spawnX = TerrainManager.MaxX - (numPlayers * _spawnOffset);
+        }
+
+        var player = Instantiate(playerPrefab);
+        player.transform.position = new Vector3(spawnX, 10, 0);
+        NetworkServer.AddPlayerForConnection(conn, player, playerControllerId);
+    }
+}
